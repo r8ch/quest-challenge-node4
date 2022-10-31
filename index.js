c145b3dd0ba315f59f47a67dafea3a7cdef821de
r8ch <81969279+r8ch@users.noreply.github.com>
2022-10-31 18:15:08 +0000

    add the solution to the challenge

---
+++ index.js
@@ -0,0 +1,6 @@
+require("dotenv").config();
+const name = process.env.MY_NAME;
+const city = process.env.MY_CITY;
+const language = process.env.MY_LANGUAGE;
+
+console.log(` I am ${name}, wilder in ${city}, and I love ${language}.`);
